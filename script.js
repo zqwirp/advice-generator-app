12a428d771f9f4fe7a118e238dd67603b6e48ea3
wiji <wiji.permana@gmail.com>
2022-05-15 18:27:12 +0700

    Commit

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
   const diceBtnElement = document.getElementById("dice-btn");
 
-  diceBtnElement.addEventListener("click", event => {
+  diceBtnElement.addEventListener("click", async event => {
     event.target.disabled = true;
-    const timeOutCounter = setInterval(() => {
 
-    }, 1000)
+    const adviceElement = document.getElementById("advice");
+    const adviceIdElement = document.getElementById("advice-id");
+    adviceElement.innerText = null;
+
+    const loader = adviceElement.nextElementSibling;
+    loader.style.display = "block";
+
+    const slipId = Math.floor(Math.random() * (100 - 1)) + 1;
+    const data = await fetch("https://api.adviceslip.com/advice/" + slipId)
+      .then(res => res.json())
+      .then(res => {
+        return {
+          id: res.slip.id,
+          advice: res.slip.advice,
+        };
+      });
+
+    const textAdvice = document.createElement("q");
+    textAdvice.innerText = data.advice;
+    adviceIdElement.innerText = data.id;
+    adviceElement.appendChild(textAdvice);
+    loader.style.display = "none";
+
     setTimeout(() => {
       event.target.disabled = false;
-    }, 10000);
+    }, 5000);
   });
 });
